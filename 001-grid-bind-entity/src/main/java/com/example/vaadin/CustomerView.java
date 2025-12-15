87e0ce1627af7acb34c9f7d6706322d2130c5cbb
Matti Tahvonen <matti@vaadin.com>
2025-12-15 17:42:36 +0200

    Upgraded to a supported version of Spring Boot (and Vaadin 25 rc2)

--- 001-grid-bind-entity/src/main/java/com/example/vaadin/CustomerView.java
+++ 001-grid-bind-entity/src/main/java/com/example/vaadin/CustomerView.java
@@ -11,7 +11,7 @@ import com.vaadin.flow.router.Route;
  */
 @Route("")
 @PageTitle("Customers")
-@Menu(title = "Customers", icon = "VAADIN_USER")
+@Menu(title = "Customers", icon = "vaadin:user")
 public class CustomerView extends VerticalLayout {
 
   public CustomerView(CustomerRepository repository) {
