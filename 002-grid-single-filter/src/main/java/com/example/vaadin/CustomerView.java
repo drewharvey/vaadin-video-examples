402cf41a71f7fbba7c7e7aed2f477e4fe3853600
Drew Harvey <drew@vaadin.com>
2025-12-16 14:38:36 -0700

    002: adding explination of why core example was changed from video code
    The video released showed filtering using dataView.addFilter(...), however, it was concluded that this technique would not be as clear for devs new to Vaadin.

--- 002-grid-single-filter/src/main/java/com/example/vaadin/CustomerView.java
+++ 002-grid-single-filter/src/main/java/com/example/vaadin/CustomerView.java
@@ -9,9 +9,24 @@ import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Strings;
+
+import java.util.Collection;
 
 /**
- * View where we display a list of all customers in a {@link Grid} component.
+ * Changes since release of 'Add filtering to Grid' video:
+ *
+ * The video demonstrates a more advanced filtering approach using a
+ * {@link com.vaadin.flow.data.provider.ListDataView}.
+ * This approach is more efficient for situations where fetching from the
+ * backend is expensive. However, to keep the example simple and focused
+ * on high-level concepts, we use a straightforward method that fetches
+ * filtered data directly from the service in this code and sets the list
+ * to the grid via {@link Grid#setItems(Collection)}.
+ *
+ * We strongly recommend you use the main code example below, but if you
+ * want to look at the example demonstrated in the video, see {@link #exampleFromVideo(CustomerService)}.
  */
 @Route("")
 @PageTitle("Customers")
@@ -68,7 +83,7 @@ public class CustomerView extends VerticalLayout {
      * get an overview of the feature, but refer to the code above for the full example.
      */
     private void minimalExample(CustomerService service) {
-        var grid = new Grid<Customer>(Customer.class);
+        var grid = new Grid<>(Customer.class);
 
         var searchField = new TextField();
         searchField.addValueChangeListener(e ->
@@ -78,6 +93,27 @@ public class CustomerView extends VerticalLayout {
         addAndExpand(grid);
 
         grid.setItems(service.findAll());
+    }
+
+    private void exampleFromVideo(CustomerService service) {
+        var grid = new Grid<>(Customer.class);
+        var dataView = grid.setItems();
+
+        var searchField = new TextField();
+        searchField.setValueChangeMode(ValueChangeMode.LAZY);
+        searchField.addValueChangeListener(e -> dataView.refreshAll());
+
+        add(searchField);
+        addAndExpand(grid);
+
+        dataView.addFilter(customer -> {
+            var searchValue = searchField.getValue();
+            return StringUtils.isEmpty(searchValue)
+                    || Strings.CI.contains(customer.getName(), searchValue)
+                    || Strings.CI.contains(customer.getEmail(), searchValue);
+        });
+
+        dataView.setItems(service.findAll());
     }
 
 }
